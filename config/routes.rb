817c258181b965711ad64d838bd6623775b431a5
hammer200911 <jdaniel231@gmail.com>
2022-05-18 10:39:12 -0300

    finalizado a tarefa

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  resources :tarefas
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :tarefas, only: [:index, :create, :update, :destroy]
+ 
+  root "tarefas#index"
 end
